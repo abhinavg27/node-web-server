95d31af71fb5ede9199820d49f40bff6e2f00db0
Abhinav Gupta <Abhinav.Gupta@247-inc.com>
2018-04-24 16:26:57 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentDate', () =>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('upper', (text) =>{
+	return text.toUpperCase();
+});
+
+app.set('view engin','hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	console.log(now);
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+
+	fs.appendFile('server.log', log + '\n', (err) =>{
+		if(err){
+			console.log('Could not write the logs in file');
+		}
+	});
+	next();
+
+});
+
+/*app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+	//res.send('<h1>Hello Express!</h1>');
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to Home Page'
+	});
+});
+
+app.get('/about', (req,res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About Page',
+		welcomeMessage: 'Welcome to About Page'
+	});
+});
+
+app.get('/bad', (req,res) => {
+	res.send({
+		errorMessage: 'Bad Page'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on 3000 port');
+});
